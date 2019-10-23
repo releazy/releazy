fa84e5ec22f1036a3fe3e5f4d6fe45a26e9c2e79
Alison Monteiro <alisonmonteiro.10@gmail.com>
2019-10-23 19:48:58 -0300

    Add .editorconfig and say goodbye to tabs

--- server.js
+++ server.js
@@ -9,29 +9,29 @@ const app = next({ dev: environment })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-	const server = new Koa()
-	const router = new Router()
-	const subdomain = new KoaSubdomain()
+  const server = new Koa()
+  const router = new Router()
+  const subdomain = new KoaSubdomain()
 
-	server.subdomainOffset = 1
+  server.subdomainOffset = 1
 
-	router.get('*', async context => {
-		const [ subdomain ] = context.state.wildcardSubdomains
+  router.get('*', async context => {
+    const [ subdomain ] = context.state.wildcardSubdomains
 
-		await handle(context.req, context.res)
-		context.response = false
-		context.subdomain = subdomain
-	})
+    await handle(context.req, context.res)
+    context.response = false
+    context.subdomain = subdomain
+  })
 
-	subdomain.use('*', router.routes())
+  subdomain.use('*', router.routes())
 
-	server.use(async (context, next) => {
-		context.res.statusCode = 200
-		await next()
-	})
+  server.use(async (context, next) => {
+    context.res.statusCode = 200
+    await next()
+  })
 
-	server.use(subdomain.routes())
-	server.listen(port, () => {
-		console.log(`> Ready on http://localhost:${port}`)
-	})
+  server.use(subdomain.routes())
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`)
+  })
 })
