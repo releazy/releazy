d73b781689020dece49598553a136f663ac96811
Alison Monteiro <alisonmonteiro.10@gmail.com>
2019-10-23 19:43:54 -0300

    Add custom subdomains support

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const Koa = require('koa')
 const next = require('next')
 const Router = require('koa-router')
+const KoaSubdomain = require('koa-subdomain');
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const environment = process.env.NODE_ENV !== 'production'
@@ -10,18 +11,26 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
 	const server = new Koa()
 	const router = new Router()
+	const subdomain = new KoaSubdomain()
 
-	router.all('*', async context => {
+	server.subdomainOffset = 1
+
+	router.get('*', async context => {
+		const [ subdomain ] = context.state.wildcardSubdomains
+
 		await handle(context.req, context.res)
 		context.response = false
+		context.subdomain = subdomain
 	})
+
+	subdomain.use('*', router.routes())
 
 	server.use(async (context, next) => {
 		context.res.statusCode = 200
 		await next()
 	})
 
-	server.use(router.routes())
+	server.use(subdomain.routes())
 	server.listen(port, () => {
 		console.log(`> Ready on http://localhost:${port}`)
 	})
